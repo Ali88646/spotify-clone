0aab2f92bc23eb00ca858b363c46edd99ff9f9b9
Your Name <you@example.com>
2025-01-03 18:39:06 +0500

    added model for uploading songs in database installed react hook form and uniqid and types for uniqid dependencies added form for uploading songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import useAuthModal from "@/hooks/ useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import React from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
   const onClick = () => {
-    // Handle Upload
+    if (!user) {
+      return authModal.onOpen();
+    }
+    //TODO: check for subscription
+    return uploadModal.onOpen();
   };
 
   return (
